a818d984faaea6c08bd159e66a9bd37a45002de3
Nick <zhongchengr509@gmail.com>
2020-06-02 08:52:04 +0800

    2-2.添加webpack配置

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const Home = require('./containers/Home')
 const port = 3000
 
 app.get('/', (req, res) => res.send(`
---
+++ webpack.server.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const nodeExternals = require('webpack-node-externals')
+console.log(path.resolve(__dirname, "node_modules"));
+module.exports = {
+  target: "node",
+  entry: "./server/src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  externals: [nodeExternals()],
+  mode: "development",
+  module: {
+    rules: [{
+      test: /\.js?/,
+      loader: "babel-loader",
+      exclude: [
+        path.resolve(__dirname, "node_modules")
+      ],
+      options: {
+        // 设置预设的编译规则
+        presets: ["@babel/preset-react", ["@babel/preset-env", {
+          targets: {
+            browsers: ["last 2 versions"]
+          }
+        }]]
+      }
+    }]
+  }
+}
